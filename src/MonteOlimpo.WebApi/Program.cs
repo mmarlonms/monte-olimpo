31570f1ddfd4f0dd475de59970c054cf46791dce
Marlon Miranda <marlon_kkkk@hotmail.com>
2019-09-12 20:42:57 -0300

    inclusão do AddMonteOlimpoConfiguration

--- src/MonteOlimpo.WebApi/Program.cs
+++ src/MonteOlimpo.WebApi/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using MonteOlimpo.Base.Extensions.Configuration;
 
 namespace MonteOlimpo.WebApi
 {
@@ -19,6 +13,7 @@ namespace MonteOlimpo.WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+             .ConfigureAppConfiguration(ConfigurationExtensions.AddMonteOlimpoConfiguration)
                 .UseStartup<Startup>();
     }
 }
