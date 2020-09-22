e3ffc9905253951bf227edf61f5e781611b3f154
Marlon Miranda da Silva <marlon_kkkk@hotmail.com>
2020-09-21 21:22:48 -0300

    Atualização de Dependências

--- src/MonteOlimpo.Domain/Models/God.cs
+++ src/MonteOlimpo.Domain/Models/God.cs
@@ -1,11 +1,9 @@
-﻿using MonteOlimpo.Base.Core.DataAnnotations;
-using MonteOlimpo.Base.Core.Domain.Model;
+﻿using MonteOlimpo.Base.Core.Domain.Model;
 
 namespace MonteOlimpo.Domain.Models
 {
     public class God : ModelBase
     {
-        //[Required(ErrorKey ="NomeInvalido", ErrorMessage ="Nome não pode ser nulo!")]
         public string Nome { get; set; }
        
         public int Idade { get; set; }
--- src/MonteOlimpo.WebApi/Program.cs
+++ src/MonteOlimpo.WebApi/Program.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MonteOlimpo.Base.Extensions.Configuration;
 
 namespace MonteOlimpo.WebApi
@@ -8,12 +9,15 @@ namespace MonteOlimpo.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-             .ConfigureAppConfiguration(ConfigurationExtensions.AddMonteOlimpoConfiguration)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+         Host.CreateDefaultBuilder(args)
+             .ConfigureWebHostDefaults(webBuilder =>
+             {
+                 webBuilder.UseStartup<Startup>();
+             }).AddMonteOlimpoConfiguration()
+            .AddMonteOlimpoLogging();
     }
 }
